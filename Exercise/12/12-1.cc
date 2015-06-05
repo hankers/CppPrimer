45769226d340fc1c9a94858d419ef19a840a7d83
hankers <hanpeiyi1@gmail.com>
2015-06-05 16:26:06 +0800

    add Exercise and Files_from_book_authors

---
+++ Exercise/12/12-1.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <list>
+#include "StrBlob.h"
+
+using namespace std;
+
+int main()
+{
+	StrBlob b1;
+	StrBlob b2 = {"a", "an", "the"};
+	b1 = b2;
+	b2.push_back("about");
+	cout << "b1.size is " << b1.size() << endl;
+	cout << "b2.size is " << b2.size() << endl;
+	
+	return 0;
+}
